84a5a7504601f548344fc4938e1284fb77f71f1f
Neng Liangpornrattana <punneng@gmail.com>
2013-06-16 22:58:11 +0700

    rename rules

--- app/cell.rb
+++ app/cell.rb
@@ -1,12 +1,12 @@
 class Cell
-  SUB_ROLES = {
+  BORN_RULES = {
     true:   Proc.new { Cell.live },
     false:  Proc.new { Cell.dead }
   }
 
-  ROLES = { 
-    true:   Proc.new { |count| SUB_ROLES[[2,3].include?(count).to_s.to_sym].call },
-    false:  Proc.new { |count| SUB_ROLES[count.eql?(3).to_s.to_sym].call }
+  RULES = { 
+    true:   Proc.new { |count| BORN_RULES[[2,3].include?(count).to_s.to_sym].call },
+    false:  Proc.new { |count| BORN_RULES[count.eql?(3).to_s.to_sym].call }
   }
 
   class << self
@@ -29,6 +29,6 @@ class Cell
 
   def next_state(neighbors)
     count = neighbors.inject(0) { |sum ,neighbor| neighbor.alive? and sum += 1; sum }
-    ROLES[self.alive?.to_s.to_sym].call(count)
+    RULES[self.alive?.to_s.to_sym].call(count)
   end
 end
